28ee539e70a90b3f1bf12d5b837a58ff7dc3ee84
Leko <leko@leko.moe>
2022-05-07 03:52:17 +0800

    passing SeveralDiscounts test

--- index.mjs
+++ index.mjs
@@ -2,9 +2,27 @@ function price(books){
     const BASE_PRICE = 8;
     const DISCOUNTS = [0, 0, 0.05, 0.1, 0.2, 0.25].map(x => 1-x);
 
-    const diffBooksLength = new Set(books).size;
+    const baskets = [];
+    for(const book of books){
+        let isBookAdded = false;
+        for(const basket of baskets){
+            if (!basket.includes(book)){
+                basket.push(book);
+                isBookAdded = true;
+                break;
+            }
+        }
+        if (!isBookAdded){
+            baskets.push([book]);
+        }
+    }
 
-    return books.length * BASE_PRICE * DISCOUNTS[diffBooksLength];
+    let totalPrice = 0;
+    for(const basket of baskets){
+        totalPrice += BASE_PRICE * basket.length * DISCOUNTS[basket.length];
+    }
+
+    return totalPrice;
 }
 
 export {price};
