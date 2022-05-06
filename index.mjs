3c4057f83501ff0c94bee0f6eb41c80faf7e03fa
Leko <leko@leko.moe>
2022-05-07 04:20:16 +0800

    Passing EdgeCases Test

--- index.mjs
+++ index.mjs
@@ -1,25 +1,30 @@
 function price(books){
     const BASE_PRICE = 8;
-    const DISCOUNTS = [0, 0, 0.05, 0.1, 0.2, 0.25].map(x => 1-x);
+    const DISCOUNTS = [0, 0, 0.05, 0.1, 0.2, 0.25];
+    const DISCOUNT_PRICES = DISCOUNTS.map((v, i) => BASE_PRICE * i * v);
+    const DISCOUNTS_DIFF = DISCOUNT_PRICES.slice(1).map((v, i) => v - DISCOUNT_PRICES[i]);
 
-    const baskets = [];
+    const baskets = [[]];
     for(const book of books){
-        let isBookAdded = false;
-        for(const basket of baskets){
-            if (!basket.includes(book)){
-                basket.push(book);
-                isBookAdded = true;
-                break;
+        let basketIndex = -1;
+        let discountsDiff = -1;
+        for(let i = 0; i < baskets.length; i++){
+            const basket = baskets[i];
+            if (!basket.includes(book) && DISCOUNTS_DIFF[basket.length] > discountsDiff){
+                discountsDiff = DISCOUNTS_DIFF[basket.length];
+                basketIndex = i;
             }
         }
-        if (!isBookAdded){
+        if (basketIndex !== -1){
+            baskets[basketIndex].push(book);
+        } else {
             baskets.push([book]);
         }
     }
 
     let totalPrice = 0;
     for(const basket of baskets){
-        totalPrice += BASE_PRICE * basket.length * DISCOUNTS[basket.length];
+        totalPrice += BASE_PRICE * basket.length * (1-DISCOUNTS[basket.length]);
     }
 
     return totalPrice;
