e4dddd07709f8399d4999d2f184c4c1849229f5a
Leko <leko@leko.moe>
2022-05-07 03:47:46 +0800

    passing SimpleDiscounts Test

--- index.mjs
+++ index.mjs
@@ -1,7 +1,10 @@
 function price(books){
     const BASE_PRICE = 8;
+    const DISCOUNTS = [0, 0, 0.05, 0.1, 0.2, 0.25].map(x => 1-x);
 
-    return books.length * BASE_PRICE;
+    const diffBooksLength = new Set(books).size;
+
+    return books.length * BASE_PRICE * DISCOUNTS[diffBooksLength];
 }
 
 export {price};
